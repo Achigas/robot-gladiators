df6b068ddf6341148af3cd1b57f079d296ff9cea
AChigas <Achigas34@gmail.com>
2020-06-04 23:13:41 -0700

    added for loop for fight

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,17 +3,19 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-console.log(enemyName, enemyHealth, enemyAttack)
-
-var fight = function() {
+var fight = function(enemyName) {
     window.alert("Welcome to Robot Gladiators!");
+
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+
+    // if player choses to fight, then fight
+      if (promptFight === "fight" || promptFight === "FIGHT") {
 
  //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
       enemyHealth = enemyHealth - playerAttack;
@@ -44,9 +46,34 @@ var fight = function() {
         else {
             window.alert(playerName + " still has " + playerHealth + " health left.");
   }
+    // if player choses to skip
+        } else if (promptFight === "skip" || promptFight === "SKIP") {
+              // confirm user wants to skip
+            var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+                  // if yes (true), leave fight
+                    if (confirmSkip) {
+                    window.alert(playerName + " has decided to skip this fight. Goodbye!");
+                // subtract money from playerMoney for skipping
+                    playerMoney = playerMoney - 2;
+                    }
+                // if no (false), ask question again by running fight() again
+            else {
+                fight();
+                }
+            window.alert(playerName + " has chosen to skip the fight!");
+
+         } else {
+                 window.alert("You need to pick a valid option. Try again!");
+        }
 };
 
 
- fight();
+    for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+        }
 
- 
+ // Game States
+// "WIN" - Player robot has defeated all enemy robots
+//    * Fight all enemy robots
+//    * Defeat each enemy robot
+// "LOSE" - Player robot's health is zero or less
