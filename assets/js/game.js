818c285428d5424d8fa5562b95513ad6fc95422f
AChigas <Achigas34@gmail.com>
2020-06-04 23:44:02 -0700

    define for loop for enemy robots

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,8 +9,13 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+window.alert("Welcome to Robot Gladiators!");
+
 var fight = function(enemyName) {
-    window.alert("Welcome to Robot Gladiators!");
+
+      // repeat and execute as long as the enemy robot is alive 
+    while(enemyHealth > 0) {
+
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -66,11 +71,13 @@ var fight = function(enemyName) {
                  window.alert("You need to pick a valid option. Try again!");
         }
 };
+}
 
-
-    for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
-        }
+        for (var i = 0; i < enemyNames.length; i++) {
+            var pickedEnemyName = enemyNames[i];
+            enemyHealth = 50;
+            fight(pickedEnemyName);
+}
 
  // Game States
 // "WIN" - Player robot has defeated all enemy robots
